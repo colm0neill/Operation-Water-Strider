ce560a8b2e634b6f7f9c3b54490d18b53d5aef46
Colm O'Neill <colmoneill89@gmail.com>
2020-04-24 23:04:07 +0100

    Second update in building UI

--- waterstrider.js
+++ waterstrider.js
@@ -1,45 +1,123 @@
 window.onload = function () {
 	
 	
-	var c = document.getElementById("graph1");
-	var ctx = c.getContext("2d");
 	
-	var w = c.width;
-	var h = c.height;
-	var dataPointsX = w/6;
-	console.log(w+" + "+h);
-	
-var gradient = ctx.createLinearGradient(0, 0, 170, 0);
-gradient.addColorStop("0", "magenta");
-gradient.addColorStop("0.5", "blue");
-gradient.addColorStop("1.0", "red");
 
-	var array = [3 , 6, 2, 56, 32, 5, 130];
+var bk = document.getElementById("backgroundCurve");
+
+var widthImg = window.innerWidth;
+var heightImg = window.innerHeight;
+console.log("Image Width is:"+widthImg+" Image Height is:"+ heightImg);
+
+
+	bk.style.height = heightImg;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+var c = document.getElementById("graph1");
+var ctx = c.getContext("2d");
+var ctxCir = c.getContext("2d");
+ctx.scale(1,1);
+	
+var w = c.width;
+var h = c.height;
+
+var dataPointsX = w/8;
+var array = [100 , 10, 20, 40, 60, 80, 100, 30];
 var largest= 0;
- var dataPointsY = [];
+var dataPointsY = [];
 
-for (i=0; i<=largest;i++){
-    if (array[i]>largest) {
-        var largest=array[i];
-    }
-}
-for(i=0;i<7;i++){
-var t = largest /100;
-var g = Math.round(h-(array[i]/t));
-dataPointsY[i] = g;
-}
-
-console.log(dataPointsY);
+	for (i=0; i<=largest;i++){
+		if (array[i]>largest) {
+			var largest=array[i];
+		}
+	}
 	
-for(i=0;i<7;i++){
-	
-ctx.moveTo(dataPointsX*i,dataPointsY[i]);
-console.log("Move 2 = "+dataPointsY[i]+" + "+ dataPointsX*i);
+	for(i=0;i<7;i++){
+		var t = largest /100;
+		var g = Math.round(h-((array[i]/t)*5.5));
+		
+		dataPointsY[i] = g;
+	}
 
-ctx.lineTo(dataPointsX*(i+1),dataPointsY[i+1]);
-console.log("line 2 = "+dataPointsY[i+1]+" + "+ dataPointsX*(i+1));
-ctx.strokeStyle = gradient;
-ctx.lineWidth = 5;
-ctx.stroke();
+	console.log(dataPointsY);
+	for(i=0;i<9;i++){
+		if((i != 0 )||(i !=9)){
+			ctx.beginPath();
+			ctx.translate(.5,.5);
+			ctx.moveTo((dataPointsX*i)+132,dataPointsY[i]);
+			ctx.lineTo((dataPointsX*(i+1)+132),dataPointsY[i+1]);
+			ctx.lineCap = "round";
+			ctx.lineWidth = 10;
+			ctx.closePath();
+			ctx.stroke();
+			
+			
+			ctxCir.beginPath();
+			//ctxCir.style = '#fff';
+			ctxCir.linewidth= 1;
+			ctxCir.arc((dataPointsX*i)+132,dataPointsY[i],10,0*Math.PI,1.5*Math.PI);
+			ctx.closePath();
+			ctxCir.stroke();
+			
+			}
+	
+		else{
+			console.log("I equal's"+ i);
+		}
+	}
+	
+var d = document.getElementById("graph2");
+var ctx2 = d.getContext("2d");
+ctx2.scale(1,1);
+	
+var w2 = d.width;
+var h2 = d.height;
+
+var dataPointsX2 = w2/8;
+var array2 = [100 , 10, 20, 40, 60, 80, 100, 30];
+var largest2= 0;
+var dataPointsY2 = [];
+
+	for (i=0; i<=largest2;i++){
+		if (array2[i]>largest2) {
+			var largest2=array2[i];
+		}
+	}
+	
+	for(i=0;i<7;i++){
+		var t = largest2 /100;
+		var g = Math.round(h2-((array2[i]/t)*4));
+		console.log("G2 "+g);
+		dataPointsY2[i] = g;
+	}
+
+	console.log(dataPointsY2);
+	for(i=0;i<7;i++){
+		if((i != 0 )||(i !=9)){
+			ctx2.beginPath();
+			
+			ctx2.moveTo((dataPointsX*i)+132,dataPointsY2[i]);
+			ctx2.lineTo((dataPointsX*i+132),h2-20);
+			ctx2.lineCap = "round";
+			ctx2.lineWidth = 20;
+			ctx.closePath();
+			ctx2.stroke();
+			
+		console.log(i+" Y "+dataPointsY2[i]);
+			}
+	
+		else{
+			console.log("I equal's"+ i);
+		}
+	}
 }
-}
+
