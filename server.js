69b8766ba723c11536ca38a7614be1af42df8bf7
Colm O'Neill <colmoneill89@gmail.com>
2020-05-18 13:43:10 +0100

    Implementing dependencies

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const flash2 = require('connect-flash');
 
 const initializePassport = require('./passport-config')
 initializePassport(
