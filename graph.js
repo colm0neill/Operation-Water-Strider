dad5db21bf8c630d4b572c4fb11e8af194bf3b2f
Colm O'Neill <colmoneill89@gmail.com>
2020-05-27 21:01:10 +0100

    Update to Graph API
    This update requestes a specific calendar and retruns all its events.

--- graph.js
+++ graph.js
@@ -1,8 +1,52 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
 var graph = require('@microsoft/microsoft-graph-client');
 require('isomorphic-fetch');
+var calendarz = require('./routes/calendar');
+
+
+
+module.exports = {
+  getUserDetails: async function(accessToken) {
+    const client = getAuthenticatedClient(accessToken);
+
+    const user = await client.api('/me').get();
+    return user;
+    
+  },
+ 
+
+  // <GetEventsSnippet>
+  getEvents: async function(accessToken) {
+    const client = getAuthenticatedClient(accessToken);
+
+    
+
+    const events = await client
+      .api('/me/calendars')
+      .select('id,name')
+      .get();
+
+    return events;
+  },
+  
+  getOneEvents: async function(accessToken) {
+    const client = getAuthenticatedClient(accessToken);
+
+    const id = calendarz.idofCalendar;
+console.log("fuck a duck"+id);
+    const oneX = await client
+      .api('/me/calendars/'+id+'/events')
+      .select('subject,start,end')
+      .orderby('createdDateTime DESC')
+      .get();
+
+    return oneX;
+
+  }
+
+ 
+};
+
+/*
 
 module.exports = {
   getUserDetails: async function(accessToken) {
@@ -11,21 +55,30 @@ module.exports = {
     const user = await client.api('/me').get();
     return user;
   },
+ 
 
   // <GetEventsSnippet>
-  getEvents: async function(accessToken) {
+  getOneEvents: async function(accessToken) {
     const client = getAuthenticatedClient(accessToken);
 
-    const events = await client
-      .api('/me/events')
-      .select('subject,organizer,start,end')
-      .orderby('createdDateTime DESC')
+    
+
+    const oneX = await client
+      .api('/me/calendar/{id}')
+      .select('id,name')
       .get();
 
-    return events;
+    return eventsOneXOne;
+
   }
   // </GetEventsSnippet>
+
+ 
 };
+
+
+}
+*/
 
 function getAuthenticatedClient(accessToken) {
   // Initialize Graph client
@@ -39,3 +92,4 @@ function getAuthenticatedClient(accessToken) {
 
   return client;
 }
+
--- routes/calendar.js
+++ routes/calendar.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var tokens = require('../tokens.js');
 var graph = require('../graph.js');
 
+var idofCalendar = '';
+
+
 
 /* GET /calendar */
 // <GetRouteSnippet>
@@ -12,7 +15,7 @@ router.get('/',
       // Redirect unauthenticated requests to home page
       res.redirect('/')
     } else {
-      let params = {
+      var params = {
         active: { calendar: true }
       };
 
@@ -42,9 +45,42 @@ router.get('/',
         req.flash('error_msg', 'Could not get an access token');
       }
 
+      
+    
+        for(var i = 0; i < events.value.length; i++){
+          if(events.value[i].name == 'ONE 2 ONE'){
+              idofCalendar = events.value[i].id;
+              exports.idofCalendar = idofCalendar;
+              console.log("idofcalendar"+idofCalendar);
+            }
+          }
+       
+      }
+      
+     
+      if (accessToken && accessToken.length > 0 && idofCalendar !== '') {
+       
+        try {
+          // Get the events
+          var eventsOneXOne = await graph.getOneEvents(accessToken, idofCalendar);
+          params.eventsOneXOne = eventsOneXOne.value;
+          console.log("I got the events Boiiii");
+        } catch (err) {
+          req.flash('error_msg', {
+            message: 'Could not fetch events',
+            debug: JSON.stringify(err)
+          });
+        }
+      } else {
+        req.flash('error_msg', 'Could not get an access token');
+      }
+
+    
+
+
       res.render('calendar', params);
     }
-  }
+  
 );
 // </GetRouteSnippet>
 
